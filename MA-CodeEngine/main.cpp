195b51d16904f2730433b775ed7e4f99801908db
Mohamed Askar <moasker20@gmail.com>
2026-08-01 04:11:15 +0300

    Add feature join string by array

--- MA-CodeEngine/main.cpp
+++ MA-CodeEngine/main.cpp
@@ -27,9 +27,21 @@ std::string JoinString(std::vector <std::string> vTokens,const  std::string& del
 	return Text.substr(0, Text.length() - delimiter.length());
 }
 
+std::string JoinString(std::string Tokens[100], int ArrLength,const std::string& delimiter)
+{
+	std::string Text;
+	for (int i = 0; i < ArrLength; i++)
+	{
+		Text = Text + Tokens[i] + delimiter;
+	}
+
+	return Text.substr(0, Text.length() - delimiter.length());
+}
+
 int main()
 {
 	std::vector <std::string> vTokens;
+	std::string Tokens[5] = { "Mohamed" ,"Ahmed" ,"Mahmoud" ,"Hessan" ,"Askar" };
 
 	vTokens.push_back("Mohamed");
 	vTokens.push_back("Ahmed");
@@ -39,6 +51,9 @@ int main()
 
 	std::cout << "Vector after join: \n";
 	std::cout << JoinString(vTokens, "\\") << "\n";
+
+	std::cout << "\nArray after join: \n";
+	std::cout << JoinString(Tokens, 5, "\\") << "\n";
 
 	system("pause > 0");
 	return 0;
