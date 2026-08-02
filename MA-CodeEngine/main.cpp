5ce8b3aa1b0450500b01ef2199c87e0b9063bbf5
Mohamed Askar <moasker20@gmail.com>
2026-08-02 15:07:13 +0300

    Add feature remove punctuation

--- MA-CodeEngine/main.cpp
+++ MA-CodeEngine/main.cpp
@@ -1,91 +1,32 @@
+//My Solution
 #include <iostream>
-#include <cctype>
 #include <string>
+#include <cctype>
 #include <vector>
 
 
-std::vector <std::string> split(std::string text, const std::string& delimiter)
+std::string RemovePunctuation(std::string text)
 {
-	int pos = 0;
-	std::vector <std::string> vTokens;
-	std::string sWord;
-
-	while ((pos = text.find(delimiter)) != std::string::npos)
-	{
-		sWord = text.substr(0, pos);
-		if (sWord != "")
-		{
-			vTokens.push_back(sWord);
-		}
-		text.erase(0, pos + delimiter.length());
-	}
-
-	if (text != delimiter)
-	{
-		vTokens.push_back(text);
-	}
-
-	return vTokens;
-}
-
-std::string LowerAllString(std::string text)
-{
+	std::string text2;
 	for (int i = 0; i < text.length(); i++)
 	{
-		text.at(i) = std::tolower(text.at(i));
-	}
-	return text;
-}
-
-std::string JoinString(std::vector <std::string> vTokens, const std::string& delimiter)
-{
-	std::string text = "";
-	for (std::string& token : vTokens)
-	{
-		text += (token + delimiter);
-	}
-	return text.substr(0, text.length() - delimiter.length());
-}
-
-
-std::string ReplaceWordInStringUsingSplit(std::string text, const  std::string& OldWord, const std::string& NewWord, bool MatchCase = true)
-{
-	std::vector <std::string> vTokens;
-	vTokens = split(text, " ");
-
-	for (std::string& token : vTokens)
-	{
-		if (MatchCase)
+		if (!std::ispunct(text.at(i)))
 		{
-			if (token == OldWord)
-			{
-				token = NewWord;
-			}
-		}
-		else
-		{
-			if (LowerAllString(token) == LowerAllString(OldWord))
-			{
-				token = NewWord;
-			}
+			text2 += text.at(i);
 		}
 	}
-	return JoinString(vTokens, " ");
+	return text2;
 }
-
 int main()
 {
-	std::string text = "Welcome to spain , spain is a nice country";
-	std::string OldWord = "Spain";
-	std::string NewWord = "Egypt";
+	std::string text = "Welcome to spain, Spain is a nice countey;Spain is very amazing.";
 
-	std::cout << "text before replace: \n" << text << "\n";
+	std::cout << "Original text: \n";
+	std::cout << text << "\n";
 
-	std::cout << "\nReplace with match case: \n";
-	std::cout << ReplaceWordInStringUsingSplit(text, OldWord, NewWord) << "\n";
-	
-	std::cout << "\nReplace with don't match case: \n";
-  	std::cout << ReplaceWordInStringUsingSplit(text, OldWord, NewWord, false) << "\n";
+	std::cout << "\ntext after remove punctuation: \n";
+	std::cout << RemovePunctuation(text) << "\n";
 
+	system("pause > 0");
 	return 0;
 }
