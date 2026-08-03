82caf90f5cdb3d021a52550be3105a6df42fdf38
Mohamed Askar <moasker20@gmail.com>
2026-08-03 18:06:40 +0300

    Add feature show all clients

--- MA-CodeEngine/main.cpp
+++ MA-CodeEngine/main.cpp
@@ -6,84 +6,111 @@
 #include <iomanip>
 #include <fstream>
 
-const std::string& ClientsFileName = "ClientsData.txt";
-
 struct stClientData
 {
 	std::string name = "";
 	std::string accNumber = "";
+	std::string pinCode = "";
 	std::string phone = "";
-	std::string PinCode = "";
 	double accBalance = 0;
 };
 
-stClientData ReadClientData()
+const std::string& ClientsFileName = "ClientsData.txt";
+
+
+std::vector <std::string> split(std::string text, const std::string delimiter = " ")
+{
+	int pos = 0; 
+	std::string sWord = "";
+	std::vector <std::string> vTokens;
+
+	while ((pos = text.find(delimiter)) != std::string::npos)
+	{
+		sWord = text.substr(0, pos);
+		if (sWord != "")
+		{
+			vTokens.push_back(sWord);
+		}
+		text.erase(0, pos + delimiter.length());
+	}
+	
+	if (text != delimiter)
+	{
+		vTokens.push_back(text);
+	}
+
+	return vTokens;
+}
+
+stClientData ConvertLineToRecord(const std::string& line, const std::string& delimiter)
 {
 	stClientData client;
+	std::vector <std::string> vClientData;
+	vClientData = split(line, delimiter);
 
-	std::cout << "Account Number: ";
-	std::getline(std::cin >> std::ws, client.accNumber);
-	std::cout << "Pin Code: ";
-	std::getline(std::cin, client.PinCode);
-	std::cout << "Name: ";
-	std::getline(std::cin, client.name);
-	std::cout << "Phone: ";
-	std::getline(std::cin, client.phone);
-	std::cout << "Account balance: ";
-	std::cin >> client.accBalance;
+	client.accNumber = vClientData[0];
+	client.pinCode = vClientData[1];
+	client.name = vClientData[2];
+	client.phone = vClientData[3];
+	client.accBalance = std::stod(vClientData[4]);
 
 	return client;
 }
 
-std::string ConvertRecodToLine(const stClientData& client, const std::string& delimiter)
+std::vector <stClientData> LoadDataFromFileToVector(const std::string& FileName)
 {
-	std::string text;
-
-	text += client.accNumber + delimiter;
-	text += client.PinCode + delimiter;
-	text += client.name + delimiter;
-	text += client.phone + delimiter;
-	text += std::to_string(client.accBalance);
-
-	return text;
-}
-
-void AddClientsToFile(const std::string& text)
-{
+	std::vector <stClientData> vClientsData;
 	std::fstream file;
-	file.open(ClientsFileName, std::ios::out | std::ios::app);
+	file.open(FileName, std::ios::in);
 
 	if (file.is_open())
 	{
-		file << text << "\n";
+		std::string line;
+		stClientData client;
+
+		while (std::getline(file, line))
+		{
+			client = ConvertLineToRecord(line, "#//#");
+			vClientsData.push_back(client);
+		}
 		file.close();
 	}
+	return vClientsData;
 }
 
-void AddNewClient()
+void PrintClientData(const stClientData& client)
 {
-	stClientData client;
-	client = ReadClientData();
-	AddClientsToFile(ConvertRecodToLine(client, "#//#"));
+	std::cout << "|" << std::left << std::setw(20) << client.accNumber;
+	std::cout << "|" << std::left << std::setw(18) << client.pinCode;
+	std::cout << "|" << std::left << std::setw(40) << client.name;
+	std::cout << "|" << std::left << std::setw(20) << client.phone;
+	std::cout << "|" << std::left << std::setw(14) << client.accBalance << "|";
 }
 
-void AddClients()
+void ShowAllClients(std::vector <stClientData>& vClientsData)
 {
-	char AddMore = 'Y';
-	do
+	std::cout << "\n\n\t\t\t\t\t\t Clients list (" << vClientsData.size() << ") (s) \n";
+	std::cout << "----------------------------------------------------------------------------------------------------------------------\n";
+	std::cout << "|" << std::left << std::setw(20) << "Account Number" << "|";
+	std::cout << std::left << std::setw(18) << "Pin Code" << "|";
+	std::cout << std::left << std::setw(40) << "Client Name" << "|";
+	std::cout << std::left << std::setw(20) << "Phone" << "|";
+	std::cout << std::left << std::setw(14) << "Balance" << "|";
+	std::cout << "\n----------------------------------------------------------------------------------------------------------------------\n";
+
+	for (stClientData& client : vClientsData)
 	{
-		system("cls");
-		AddNewClient();
-
-		std::cout << "\nClient added successfully.";
-		std::cout << "\nDo yo want to add more clients[Y,N]: ";
-		std::cin >> AddMore;
-
-	} while (std::tolower(AddMore) == 'y');
+		PrintClientData(client);
+		std::cout << std::endl;
+	}
+	std::cout << "----------------------------------------------------------------------------------------------------------------------" << std::endl;
 }
 
 int main()
 {
-	AddClients();
+	std::vector <stClientData> vClientData = LoadDataFromFileToVector(ClientsFileName);
+	ShowAllClients(vClientData);
+
+
 	return 0;
 }
